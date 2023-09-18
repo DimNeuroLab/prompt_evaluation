2fcd4a3ddb84816e7948337a053654500dcd97bb
Gregor Donabauer <gregor.donabauer@gmx.de>
2023-09-18 16:11:04 +0200

    Initial commit

---
+++ evaluate_chatgpt.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
+
+
+def get_prompt_wise_scores(true_df, pred_df, filler=0):
+    accuracy_scores = []
+    f1_scores = []
+    for idx, row in true_df.iterrows():
+        y_true = row.values[1:]
+        y_pred = pred_df.iloc[idx].values[1:]
+        y_pred[y_pred == -1] = filler
+        y_true = list(y_true)
+        y_pred = list(y_pred)
+        accuracy_scores.append(accuracy_score(y_true, y_pred))
+        f1_scores.append(f1_score(y_true, y_pred, average='macro'))
+    return accuracy_scores, f1_scores
+
+
+def get_feature_wise_scores(true_df, pred_df, filler=0):
+    accuracy_scores = {}
+    f1_scores = {}
+    for feature in list(true_df.columns)[1:]:
+        y_true = true_df[feature].tolist()
+        y_pred = pred_df[feature].to_numpy()
+        y_pred[y_pred == -1] = filler
+        accuracy_scores[feature] = accuracy_score(y_true, y_pred)
+        f1_scores[feature] = f1_score(y_true, y_pred, average='macro')
+    return accuracy_scores, f1_scores
+
+
+if __name__ == '__main__':
+    ANNOTATIONS = pd.read_csv('annotations.tsv', sep='\t')
+    CHAT_GPT = pd.read_csv('chatgpt_evaluation.tsv', sep='\t')
+
+    # block below for feature-wise evaluation
+    accuracy_scores, f1_scores = get_feature_wise_scores(ANNOTATIONS, CHAT_GPT)
+    print('ACC', accuracy_scores, sep='\n')
+    print('F1', f1_scores, sep='\n')
+
+    # uncomment block below for prompt-wise evaluation
+    '''
+    accuracy_scores, f1_scores = get_prompt_wise_scores(ANNOTATIONS, CHAT_GPT)
+
+    print(accuracy_scores)
+    print('ACC', sum(accuracy_scores)/(len(accuracy_scores)))
+    print(f1_scores)
+    print('F1', sum(f1_scores)/(len(f1_scores)))
+    '''
---
+++ main.py
@@ -0,0 +1,87 @@
+import sys
+from tqdm import tqdm
+import pandas as pd
+import json
+import numpy as np
+import openai
+
+
+FEATURES = pd.read_csv('features.tsv', sep='\t')
+ANNOTATIONS = pd.read_csv('annotations.tsv', sep='\t')
+
+openai.api_key = '' # add key here
+model_name = "gpt-3.5-turbo"
+
+
+def get_positive_few_shot_example(feature_name, prompt):
+    relevant = ANNOTATIONS[['prompt', feature_name]]
+    try:
+        rel_rows = relevant.loc[(relevant['prompt'] != prompt) & (relevant[feature_name] == 1)]
+        return rel_rows.sample(1)['prompt'].iloc[0]
+    except:
+        return ''
+
+
+def get_negative_few_shot_example(feature_name, prompt):
+    relevant = ANNOTATIONS[['prompt', feature_name]]
+    try:
+        rel_rows = relevant.loc[(relevant['prompt'] != prompt) & (relevant[feature_name] == 0)]
+        return rel_rows.sample(1)['prompt'].iloc[0]
+    except:
+        return ''
+
+def evaluate_prompt(eval_prompt, debug=True):
+    feature_list = FEATURES['feature_name'].tolist()
+    prompt_annotations = []
+    prompt_annotations.append(eval_prompt)
+
+    for feature in feature_list:
+        feature_description = FEATURES.loc[FEATURES['feature_name'] == feature]['prompt_command'].iloc[0]
+        # include = FEATURES.loc[FEATURES['feature_name'] == feature]['include'].iloc[0]
+        positive_few_shot = get_positive_few_shot_example(feature, eval_prompt)
+        negative_few_shot = get_negative_few_shot_example(feature, eval_prompt)
+
+        eval_string = f"""Given the following feature:
+        {feature_description}\n
+        The feature is present in the following prompt:
+        {positive_few_shot}\n
+        The feature is not present in the following prompt:
+        {negative_few_shot}\n
+        Tell me whether the feature is present in the prompt given below. Formalize your output as a json object, where the key is the feature description and the associated value is 1 if the feature is present or 0 if not.\n
+        Prompt:
+        {eval_prompt}"""
+        conversation = [{'role': 'system', 'content': eval_string}]
+
+        if debug:
+            print(50*'*', feature)
+            print(eval_string)
+            response = {feature_description: 1}
+        else:
+            response = openai.ChatCompletion.create(
+                model=model_name,
+                messages=conversation
+            )
+            response = json.loads(response['choices'][0]['message']['content'])
+
+        try:
+            response_value = int(response[feature_description])
+        except:
+            response_value = -1
+        prompt_annotations.append(response_value)
+
+    return prompt_annotations
+
+
+if __name__ == '__main__':
+
+    df_column_names = list(ANNOTATIONS.columns)
+    df_values = []
+
+    prompts = ANNOTATIONS['prompt'].tolist()
+    for prompt in tqdm(prompts):
+        # set debug=False to do actual API calls
+        prompt_annotations = evaluate_prompt(prompt, debug=True)
+        df_values.append(prompt_annotations)
+
+    result_data = pd.DataFrame(np.array(df_values), columns=df_column_names)
+    result_data.to_csv('chatgpt_evaluation.tsv', sep='\t', index=False)
