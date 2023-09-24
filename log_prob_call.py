1c4fd1a4e9314cc7fc22314fecf61c0b0c8ba816
dimitri-ognibene <13626970+dimitri-ognibene@users.noreply.github.com>
2023-09-24 12:50:19 +0200

    new code for multiple random shots

--- log_prob_call.py
+++ log_prob_call.py
@@ -22,12 +22,13 @@ ANNOTATIONS = pd.read_csv('data/'+annotation_filename+'.tsv', sep='\t')
 
 openai.api_key = get_api_key()
 model_name =   "gpt-3.5-turbo-instruct" #'text-davinci-003' # "gpt-3.5-turbo" # #"gpt-4"
-promptCreator=3
+promptCreator=4
+shots=3
 num_runs= 11
 
 
 
-@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(3))
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(4))
 def completion_with_backoff(**kwargs):
     return openai.Completion.create(**kwargs)
 
@@ -208,6 +209,90 @@ def createPromptInverted(eval_prompt, feature,shots):
     return eval_string,feature_description
 
 
+def createPromptRandom(eval_prompt, feature, shots):
+    '''
+
+    :param eval_prompt:
+    :param feature:
+    :param shots:
+    :return:
+
+    output example (sequence of labels used in the few shot learning: N Y Y N)
+            Me: Answer with Yes or No if this feature:
+            additional contextual information about the role of the language model, the user, or the environment
+
+            is present in the following prompt:
+
+            Explain the negative sides of social media use without using bulletins and ask one question at a time. And make it interactive by asking questions like a teacher
+
+            You: No
+
+            Me: and in the following prompt?
+
+            I'm a student!  Could you be my super-cool "teacher" for a bit and chat about two tricky things with social media "Echo Chambers" and "Social Media Self Protection Skills"?  Here's how we can make it awesome:   - Let's make this a real conversation. You ask one question at a time, always hold up for my reply, I answer, and go to the next interactive step.  - Keep the conversation fun! A joke or two wouldn't hurt.  - First, what is my name, how old am I and what's my school level? That way, you can keep things more appropriate for me.  - Lastly, what's my cultural background? It'll help make our chat about social media even more understandable by mentioning related examples specific to my culture for each topic.
+
+            You: Yes
+
+
+            Me: and in the following prompt?
+
+            Hello! Please try to act like my teacher teaching me disadvantages of social media by considering my age, level of education, and culture but in a more friendly and supportive way. Meanwhile, please do this in an interactive way by asking one question at a time.
+
+            You: Yes
+
+            Me: and in the following prompt?
+
+            I want you to teach me the disadvantages of social media according to my personal information like age, level of education, & culture.
+
+            You: No
+
+
+            Me: and in the following prompt?
+
+            Hello! I want to learn more about the negative aspects of social media. Can we have an educative conversation about it?
+
+            You:
+    '''
+    feature_description = FEATURES.loc[FEATURES['feature_name'] == feature]['prompt_command'].iloc[0]
+    # include = FEATURES.loc[FEATURES['feature_name'] == feature]['include'].iloc[0]
+    positive_few_shot = []
+    negative_few_shot = []
+    eval_string = f"""Me: Answer with Yes or No if this feature:
+                {feature_description}\n
+                is present in the following prompt:\n
+                """
+    for i in range(shots):
+        positive_few_shot.append(get_positive_few_shot_example(feature, eval_prompt, shots=1))
+        # positive_few_shot = ['Prompt ' + str(idx + 1) + ': ' + val for idx, val in enumerate(positive_few_shot)]
+        positive_few_shot[i] = '\n'.join(positive_few_shot[i])
+        negative_few_shot.append(get_negative_few_shot_example(feature, eval_prompt, shots=1))
+    # negative_few_shot = ['Prompt ' + str(idx + 1) + ': ' + val for idx, val in enumerate(negative_few_shot)]
+        negative_few_shot[i] = '\n'.join(negative_few_shot[i])
+        if np.random.choice(2, 1)==1:
+            newsubstring=f"""
+            {negative_few_shot[i]}\n
+            You: No\n
+            Me: and in the following prompt?\n
+            {positive_few_shot[i]}\n
+            You: Yes\n
+            Me: and in the following prompt?\n"""
+            eval_string+=newsubstring
+        else:
+            eval_string+=f""""
+            {positive_few_shot[i]}\n
+            You: Yes\n
+            Me: and in the following prompt?\n
+            {negative_few_shot[i]}\n
+            You: No\n
+            Me: and in the following prompt?\n"""
+
+    eval_string+=f""""
+    {eval_prompt}\n
+    You: \n"""
+
+    return eval_string, feature_description
+
+
 def createPromptRevised(eval_prompt, feature, shots):
     feature_description = FEATURES.loc[FEATURES['feature_name'] == feature]['prompt_command'].iloc[0]
     # include = FEATURES.loc[FEATURES['feature_name'] == feature]['include'].iloc[0]
@@ -257,6 +342,9 @@ def evaluate_prompt_logits(eval_prompt, debug=True, shots=1,promptCreator=2):
             eval_string, feature_description = createPromptInverted(eval_prompt, feature, shots)
         elif promptCreator==3:
             eval_string, feature_description = createPromptRevised(eval_prompt, feature, shots)
+        elif promptCreator==4:
+            eval_string, feature_description = createPromptRandom(eval_prompt, feature, shots)
+
         conversation = [{'role': 'system', 'content': eval_string}]
         print('*'*15 + "  eval string  "+'*'*15)
         print(eval_string)
@@ -270,7 +358,7 @@ def evaluate_prompt_logits(eval_prompt, debug=True, shots=1,promptCreator=2):
             for _ in range(max_attempts):
                 try:
                     time.sleep(0.5)
-                    with timeoutWindows(seconds=20):
+                    with timeoutWindows(seconds=40):
                         '''
                         response = openai.ChatCompletion.create(
                             model=model_name,
@@ -380,8 +468,9 @@ if __name__ == '__main__':
         prompts = ANNOTATIONS['prompt'].tolist()
         for prompt in tqdm(prompts):
             # set debug=False to do actual API calls
-            prompt_annotations = evaluate_prompt_logits(prompt, debug=False, shots=1,promptCreator=promptCreator)
+            prompt_annotations = evaluate_prompt_logits(prompt, debug=False, shots=shots,promptCreator=promptCreator)
             df_values.append(prompt_annotations)
+
 
 
 
@@ -392,5 +481,5 @@ if __name__ == '__main__':
 
         timestr = time.strftime("%Y%m%d-%H%M%S")
         result_data = pd.DataFrame(np.array(df_values), columns=df_column_names)
-        result_data.to_csv('output/'+model_name+'_evaluation_log_2shots_promptgen_'+str(promptCreator)+"_features_file_"+features_filename+"_annotation_file_"+annotation_filename+'_'+timestr+'.tsv', sep='\t', index=False)
+        result_data.to_csv('output/'+model_name+'_evaluation_log_shots_'+str(shots)+'promptgen_'+str(promptCreator)+"_features_file_"+features_filename+"_annotation_file_"+annotation_filename+'_'+timestr+'.tsv', sep='\t', index=False)
 
