222c5444b847e1edda94465c18625d936da4d3b5
dimitri-ognibene <13626970+dimitri-ognibene@users.noreply.github.com>
2023-09-24 11:53:52 +0200

    created overall document

--- evaluate_chatgpt_prob.py
+++ evaluate_chatgpt_prob.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import glob
 from sklearn.metrics import accuracy_score, f1_score
 
 
@@ -17,9 +18,11 @@ def get_prompt_wise_scores(true_df, pred_df, filler=0):
     return accuracy_scores, f1_scores
 
 
-def get_feature_wise_scores(true_df, pred_df, filler=0):
+def get_feature_wise_scores(true_df, pred_df, filename, filler=0):
     accuracy_scores = {}
     f1_scores = {}
+    accuracy_scores['run'] = f
+    f1_scores['run'] = f
     for feature in list(true_df.columns)[1:]:
         y_true = true_df[feature].tolist()
         y_pred = pred_df[feature+'_Y'].to_numpy()
@@ -35,20 +38,37 @@ def get_feature_wise_scores(true_df, pred_df, filler=0):
 
 
 if __name__ == '__main__':
-    ANNOTATIONS = pd.read_csv('data/annotations.tsv', sep='\t')
-    CHAT_GPT = pd.read_csv('output/gpt-3.5-turbo-instruct_evaluation_log_2shots_promptgen_2_20230923-145901.tsv', sep='\t')
 
-    # block below for feature-wise evaluation
-    accuracy_scores, f1_scores = get_feature_wise_scores(ANNOTATIONS, CHAT_GPT)
-    print('ACC', accuracy_scores, sep='\n')
-    print('F1', f1_scores, sep='\n')
+    ANNOTATIONS = pd.read_csv('data/annotations.tsv', sep='\t')
+    files = glob.glob("output/*log_2shots*.tsv")
+    features_results_accuracy=[]
+    features_results_f1 = []
+    prompt_results_accuracy = []
+    prompt_results_f1 = []
+
+    for f in files:
+        CHAT_GPT = pd.read_csv(f, sep='\t')
+
+        # block below for feature-wise evaluation
+        accuracy_scores, f1_scores = get_feature_wise_scores(ANNOTATIONS, CHAT_GPT,f)
+        print('ACC', accuracy_scores, sep='\n')
+        print('F1', f1_scores, sep='\n')
+        features_results_f1.append(f1_scores)
+        features_results_accuracy.append(accuracy_scores)
+
+    result_data = pd.DataFrame(features_results_f1)
+    result_data.to_csv('output\evaluation_prob_runs_f1.tsv',
+                           sep='\t', index=False)
+    result_data = pd.DataFrame(features_results_accuracy)
+    result_data.to_csv('output\evaluation_prob_runs_accuracy.tsv',
+                       sep='\t', index=False)
 
     # uncomment block below for prompt-wise evaluation
     '''
-    accuracy_scores, f1_scores = get_prompt_wise_scores(ANNOTATIONS, CHAT_GPT)
-
-    print(accuracy_scores)
-    print('ACC', sum(accuracy_scores)/(len(accuracy_scores)))
-    print(f1_scores)
-    print('F1', sum(f1_scores)/(len(f1_scores)))
+        accuracy_scores, f1_scores = get_prompt_wise_scores(ANNOTATIONS, CHAT_GPT)
+    
+        print(accuracy_scores)
+        print('ACC', sum(accuracy_scores)/(len(accuracy_scores)))
+        print(f1_scores)
+        print('F1', sum(f1_scores)/(len(f1_scores)))
     '''
